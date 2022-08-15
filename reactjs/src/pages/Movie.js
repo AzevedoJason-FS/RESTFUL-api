d78e96f71e082ca0b5bbb046d8aa4a85fa671cb2
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-08-15 14:23:06 -0400

    Fixed map

--- reactjs/src/pages/Movie.js
+++ reactjs/src/pages/Movie.js
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from 'react-router-dom';
+
 import '../App.css';
-import React, { useEffect, useState } from "react"
-import {Link, useParams, useNavigate } from "react-router-dom"
 
 function Movie() {
   const [movies, setMovies] = useState(null)
@@ -16,16 +17,18 @@ function Movie() {
   const navigate = useNavigate();
 
   const API_BASE = process.env.NODE_ENV === 'development' ? `http://localhost:8000/api/v1` : process.env.REACT_APP_BASE_URL;
+  let ignore = false;
 
   useEffect(() => {
-    let ignore = false;
+ 
     if(!ignore){
       getMovie();
     }
+
     return () => {
       ignore = true;
     }
-  }, [])
+  },[])
 
   const getMovie = async () => {
     setLoading(true)
@@ -41,15 +44,15 @@ function Movie() {
         })
       })
     }catch(error){
-      setError(error.message)
-    } finally{
+      setError(error.message || "Unexpected Error")
+    } finally {
       setLoading(false)
     }
   }
 
   const deleteMovie = async () => {
     try{
-        await fetch(`${API_BASE}/movies/${id}`,{
+        await fetch(`${API_BASE}/movies/${id}`, {
             method: 'DELETE'
         })
         .then(res => res.json())
@@ -58,15 +61,15 @@ function Movie() {
           navigate("/dashboard", { replace: true })
         })
       }catch(error){
-        setError(error.message)
-      } finally{
+        setError(error.message || "Unexpected Error")
+      } finally {
         setLoading(false)
       }
   }
 
   const updateMovie = async () => {
     try{
-        await fetch(`${API_BASE}/movies/${id}`,{
+        await fetch(`${API_BASE}/movies/${id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
@@ -75,52 +78,49 @@ function Movie() {
         })
         .then(res => res.json())
         .then(data => {
-          setMovies(data)
-          navigate("/dashboard", { replace: true })
+          console.log({data})
         })
       }catch(error){
-        setError(error.message)
-      } finally{
+        setError(error.message || "Unexpected Error")
+      } finally {
         setLoading(false)
       }
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    updateMovie();
+      e.preventDefault();
+      updateMovie();
   }
 
-  const handleInputChange = (e) => {
-    e.persist();
-    setValues((values) => ({
-        ...values,
-        [e.target.name]: e.target.value
-    }))
+  const handleInputChanges = (e) => {
+      e.persist();
+      setValues((values) => ({
+          ...values,
+          [e.target.name]:e.target.value
+      }))
   }
 
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Movie: {values && values.name} </h2>
-       <button onClick={() => deleteMovie()}>Delete Movie</button>
-       <Link to="/">Home</Link>
-       <Link to="/dashboard">Dashboard</Link>
+        <h1>Movie Profile</h1>
+        <h2>{values && values.name}</h2>
+        <h2>{values && values.collection_Name}</h2>
+        <button onClick={() => deleteMovie()}> Delete Movie</button>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
 
-       <form onSubmit={(e) => handleSubmit(e)}>
-           <label>
-               Name:
-               <input type="text" name="name" value={values.name} onChange={handleInputChange} />
-           </label>
-           <label>
-               Collection Name:
-               <input type="text" name="collection_Name" value={values.collection_Name} onChange={handleInputChange} />
-           </label>
-           <label>
-               Year:
-               <input type="text" name="year" value={values.year} onChange={handleInputChange} />
-           </label>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <label>
+                Name:
+                <input type="text" name="name" value={values.name} onChange={handleInputChanges} />
+            </label>
+            <label>
+                Name:
+                <input type="text" name="collection_Name" value={values.collection_Name} onChange={handleInputChanges} />
+            </label>
             <input type="submit" value="Submit" />
-       </form>
+        </form>
       </header>
     </div>
   );
