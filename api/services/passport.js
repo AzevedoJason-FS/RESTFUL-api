9cd4f3b68d24be4afda9ca59579a87668125c95d
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-08-26 19:44:34 -0400

    User login authentication

--- api/services/passport.js
+++ api/services/passport.js
@@ -1,9 +1,25 @@
 const passport = require('passport');
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const StrategyJwt = require('passport-jwt').Strategy;
-
+const LocalStrategy = require('passport-local');
 const User = require('../models/user');
 const config = require('../config');
+
+const localOptions = {
+    usernameField: 'email'
+}
+
+const localStrategy = new LocalStrategy(localOptions, function(email, password, done){
+    User.findOne({email: email}, function(error, user){
+        if(error) { return done(error) }
+        if(!user) { return done(null, false) }
+        user.comparePassword(password, function(error, isMatch){
+            if(error) { return done(error) }
+            if(!isMatch) { return done(null, false) }
+            return done(null, user);
+        })
+    })
+})
 
 const jwtOptions = {
     secretOrKey: config.secret,
@@ -21,4 +37,5 @@ const JwtStrategy = new StrategyJwt(jwtOptions, function(payload, done){
     })
 })
 
+passport.use(localStrategy)
 passport.use(JwtStrategy)
